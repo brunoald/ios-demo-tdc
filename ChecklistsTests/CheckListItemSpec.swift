9033ba087540e41fbdb7d87ecac2c4ff204f660a
Bruno Dias <bdias@thoughtworks.com>
2018-10-18 19:03:40 -0300

    Adiciona testes para o método init da classe CheckListItem

---
+++ ChecklistsTests/CheckListItemSpec.swift
@@ -0,0 +1,28 @@
+import Quick
+import Nimble
+@testable import Checklists
+
+class ChecklistItemSpec: QuickSpec {
+    override func spec() {
+        describe("ChecklistItem") {
+            var item: ChecklistItem!
+            
+            beforeEach {
+                item = ChecklistItem(text: "Test", checked: true)
+            }
+            
+            it("sets text value on init") {
+                expect(item.text).to(equal("Test"))
+            }
+            
+            it("sets checked value on init") {
+                expect(item.checked).to(beTrue())
+            }
+            
+            it("can be unchecked") {
+                item.toggleChecked()
+                expect(item.checked).to(beFalse())
+            }
+        }
+    }
+}
