787c91a5d2ea341561a219fd188f280c2c6ba27b
Bruno Dias <bdias@thoughtworks.com>
2018-10-19 11:09:59 -0300

    Cria mock para ItemDetailViewControllerDelegate

---
+++ ChecklistsTests/Mocks/ItemDetailViewControllerDelegateMock.swift
@@ -0,0 +1,19 @@
+@testable import Checklists
+
+class ItemDetailViewControllerDelegateMock: ItemDetailViewControllerDelegate {
+    var didCallNewItemAdded = false
+    var didCallActionCancelled = false
+    var didCallItemEdited = false
+    
+    func newItemAdded() {
+        didCallNewItemAdded = true
+    }
+    
+    func actionCancelled() {
+        didCallActionCancelled = true
+    }
+    
+    func itemEdited() {
+        didCallItemEdited = true
+    }
+}
