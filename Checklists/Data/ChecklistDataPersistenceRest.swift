12742139e42b48dc81d07c990d51359bf1e131e2
Bruno Dias <bdias@thoughtworks.com>
2018-10-22 18:44:41 -0300

    Utilização de serviço na nuvem

--- Checklists/Data/ChecklistDataPersistenceRest.swift
+++ Checklists/Data/ChecklistDataPersistenceRest.swift
@@ -3,17 +3,18 @@ import Alamofire_Synchronous
 import SwiftyJSON
 
 class ChecklistDataPersistenceRest: ChecklistDataPersistenceDelegate {
+    let baseUrl = "https://ios-tdc-demo-api.herokuapp.com"
     func saveChecklistItems(items: [ChecklistItem]) {
         let parameters: Parameters = [
             "items": items.map({
                 [ "text": $0.text, "checked": $0.checked ]
             })
         ]
-        _ = Alamofire.request("http://localhost:3000/items.json", method: .post, parameters: parameters).responseData()
+        _ = Alamofire.request("\(baseUrl)/items.json", method: .post, parameters: parameters).responseData()
     }
     
     func loadChecklistItems() -> [ChecklistItem] {
-        let response = Alamofire.request("http://localhost:3000/items.json").responseJSON()
+        let response = Alamofire.request("\(baseUrl)/items.json").responseJSON()
         let json = JSON(response.result.value)
         return json.arrayValue.map({
             ChecklistItem(text: $0["text"].stringValue, checked: $0["checked"].boolValue)
